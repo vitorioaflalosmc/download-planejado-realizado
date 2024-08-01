f2fb7b465e262a2d3d76c365d6248e4e206bb2ec
vitorioaflalo <vitorio.aflalo@santamarcelinacultura.org.br>
2024-08-01 09:54:55 -0300

    atualizando as credenciais

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-USERNAME = "vitorio.aflalo"
-PASSWORD = "smc@2024"
+USERNAME = "" #INSIRA SEU LOGIN DO SPM AQUI ENTRE ASPAS "LOGIN"
+PASSWORD = "" #INSIRA SUA SENHA DO SPM AQUI ENTRE ASPAS "SENHA"
 EDGE_DRIVER_PATH = "C:/Users/vitorio.aflalo/Desktop/Automacoes/scripts-python/webdriver/msedgedriver.exe"
